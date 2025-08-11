2df534c079f8da08c33d58defcaef42b218ea06a
Josiah <112897472+jojefvz@users.noreply.github.com>
2025-08-11 12:55:54 -0500

    Create init_schemas.sql

---
+++ scripts/init_schemas.sql
@@ -0,0 +1,15 @@
+/*
+=============================================================
+Create Schemas
+=============================================================
+Script Purpose:
+    This script creates three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+*/
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
